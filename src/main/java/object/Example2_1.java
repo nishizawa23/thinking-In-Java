3c24d60d4d716e4002dbf52a068cdd1e7371712e
Hunagxh <witshxh@163.com>
2016-12-26 07:02:20 +0000

    Add Example 2.1

---
+++ src/main/java/object/Example2_1.java
@@ -0,0 +1,13 @@
+public class Example2_1 {
+
+	private int i;
+	private char a;
+	private Example2_1 mExample;
+
+	public static void main (String [] args)
+	{
+		/* code */
+		Example2_1 mExample = new Example2_1();
+		System.out.println("Example2_1 int i : " +mExample.i + " char a : " +mExample.a);
+	}
+}
