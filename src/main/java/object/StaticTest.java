062389db84fdba8da2f33f5a8898cb8fdaf88707
Hunagxh <witshxh@163.com>
2016-12-26 07:58:03 +0000

    Update StaticTest for example 2.8

--- src/main/java/object/StaticTest.java
+++ src/main/java/object/StaticTest.java
@@ -10,5 +10,15 @@ public class StaticTest{
 		/* code */
 		StaticTest.increment();
 		System.out.println("Run static method increment() : " + StaticTest.i);
+
+		StaticTest mST1 = new StaticTest();
+		StaticTest mST2 = new StaticTest();
+
+		mST1.i = 99;
+		System.out.println("mST1.i is " + mST1.i);
+		System.out.println("mST2.i is " + mST2.i);
+		mST1.i = 1000;
+		System.out.println("mST1.i is " + mST1.i);
+		System.out.println("mST2.i is " + mST2.i);
 	}
 }
