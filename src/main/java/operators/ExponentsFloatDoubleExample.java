d5a970642b61a85ecd9c6bd268057c41b9b5275c
pete <nishizawa23@gmail.com>
2017-01-02 23:34:31 +0800

    Add ExponentsFloatDoubleExmaple for 3.9

---
+++ src/main/java/operators/ExponentsFloatDoubleExample.java
@@ -0,0 +1,18 @@
+package operators; 
+
+import java.util.*;
+
+public class ExponentsFloatDoubleExample {
+	public static void main (String [] args)
+	{
+		/* code */
+		float mFloatMax = Float.MAX_VALUE;
+		float mFloatMin = Float.MIN_VALUE;
+		double mDoubleMax = Double.MAX_VALUE;
+		double mDoubleMin = Double.MIN_VALUE;
+
+		System.out.println("float max : " + mFloatMax +
+			" float min : " + mFloatMin + " double max : " + mDoubleMax
+			+ " double min : " + mDoubleMin);
+	}
+}
