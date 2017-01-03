cf057a91c53cecb9bbc2bc43e55bbf9cc3b7ff3d
Hunagxh <witshxh@163.com>
2017-01-03 06:50:09 +0000

    Updata BitManipulationShiftExampleOne for example 3.12

--- src/main/java/operators/BitManipulationShiftExampleOne.java
+++ src/main/java/operators/BitManipulationShiftExampleOne.java
@@ -17,5 +17,18 @@ public class BitManipulationShiftExampleOne {
 		for(int i = 0; i < testLength-1 ; i++){
 			System.out.println(" test int >> 1 : " + Integer.toBinaryString(test>>=1));
 		}
+
+		int test2 = 0x8AC9575A;
+		System.out.println(" test2 int : " + test2);
+		System.out.println(" test2 int binary : " + Integer.toBinaryString(test2));
+
+		System.out.println(" test2 int binary << 4 : " + Integer.toBinaryString(test2<<4));
+		System.out.println(" test2 int : " + (test2<<=4));
+		testLength = Integer.toBinaryString(test2).length();
+
+		for(int i = 0; i < testLength-1 ; i++){
+			System.out.println(" test2 int >>> 1 : " + Integer.toBinaryString(test2>>>=1));
+		}
+
 	}
 }
