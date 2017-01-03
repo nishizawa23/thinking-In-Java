0b445e5e2ab59e9ae229c5fbc2e15bf935a8a920
pete <nishizawa23@gmail.com>
2017-01-03 23:59:08 +0800

    Add NumberPrint.java for example 4.1

---
+++ src/main/java/control/NumberPrint.java
@@ -0,0 +1,15 @@
+package control;
+
+import java.util.*;
+
+public class NumberPrint {
+	public static void main (String [] args)
+	{
+		/* code */
+		System.out.println("for 1 to 100");
+
+		for(int i = 0; i<100; i++){
+			System.out.println(" i = " + (i+1));
+		}
+	}
+}
