0f1841c5c66e356438a95a5973487bcf5eec6448
pete <nishizawa23@gmail.com>
2017-01-05 23:56:02 +0800

    Add ClassA.java for example 5.7

---
+++ src/main/java/initialization/ClassA.java
@@ -0,0 +1,12 @@
+package initialization;
+
+import java.util.*;
+
+public class ClassA {
+	public static void main (String [] args)
+	{
+		/* code */
+		ClassA mA = new ClassA();
+		System.out.println("ClassA toString : " + mA.toString());
+	}
+}
