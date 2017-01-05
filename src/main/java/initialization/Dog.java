344ef6064ebc21a59da8c920f665f81c3a628f5c
pete <nishizawa23@gmail.com>
2017-01-05 23:11:29 +0800

    Update Dog.java for example 5.6

--- src/main/java/initialization/Dog.java
+++ src/main/java/initialization/Dog.java
@@ -4,19 +4,21 @@ import java.util.*;
 
 public class Dog {
 
-	public void bark(char c){
-		System.out.println("Overload bark method char " + (char)c);
+	public void bark(char c, int i){
+		System.out.println("Overload bark method char " + 
+			(char)c + "int " + i);
 	}
 
-	public void bark(int i){
-		System.out.println("Overload bark method integer" + i);
+	public void bark(int i, char c){
+		System.out.println("Overload bark method integer" + 
+			i + " char " + (char)c);
 	}
 
 	public static void main (String [] args)
 	{
 		/* code */
 		Dog mDog = new Dog();
-		mDog.bark('h');
-		mDog.bark(88);
+		mDog.bark('h',100);
+		mDog.bark(88,'c');
 	}
 }
