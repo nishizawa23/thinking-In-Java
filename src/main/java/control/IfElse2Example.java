59377b26a0cdeb22735e8fde0d034092b0890a4f
Hunagxh <witshxh@163.com>
2017-01-05 02:03:31 +0000

    Add ifElse2Example.java for example 4.6

---
+++ src/main/java/control/IfElse2Example.java
@@ -0,0 +1,18 @@
+//: control/IfElse2.java
+package control; /* Added by Eclipse.py */
+import static net.mindview.util.Print.*;
+
+public class IfElse2Example {
+  static boolean test(int testval, int begin, int end) {
+	return (testval<=end && testval>= begin)?true:false;
+  }
+  public static void main(String[] args) {
+    print(test(10, 5, 9));
+    print(test(5, 10, 12));
+    print(test(8, 5, 8));
+  }
+} /* Output:
+1
+-1
+0
+*///:~
