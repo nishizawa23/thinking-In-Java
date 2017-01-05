f0f6ef430dc879b0d2d332863b0a419816be25a9
pete <nishizawa23@gmail.com>
2017-01-05 22:46:20 +0800

    Add constructuorExample.java for example 5.3

---
+++ src/main/java/initialization/ConstructorExample.java
@@ -0,0 +1,15 @@
+package initialization;
+
+import java.util.*;
+
+public class ConstructorExample {
+
+	ConstructorExample(){
+		System.out.println("ConstructorExample constructor");
+	}
+	public static void main (String [] args)
+	{
+		/* code */
+		ConstructorExample mClass = new ConstructorExample();
+	}
+}
