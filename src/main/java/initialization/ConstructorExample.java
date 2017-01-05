89a7ada165c28bbbdc519d5ced3511a7eb6ef63d
pete <nishizawa23@gmail.com>
2017-01-05 22:49:01 +0800

    Update ConstructorExample.java for example 5.4

--- src/main/java/initialization/ConstructorExample.java
+++ src/main/java/initialization/ConstructorExample.java
@@ -7,9 +7,14 @@ public class ConstructorExample {
 	ConstructorExample(){
 		System.out.println("ConstructorExample constructor");
 	}
+
+	ConstructorExample(String s){
+		System.out.println("ConstructorExample constructor String " + s);
+	}
 	public static void main (String [] args)
 	{
 		/* code */
 		ConstructorExample mClass = new ConstructorExample();
+		ConstructorExample mClassString = new ConstructorExample("hello");
 	}
 }
