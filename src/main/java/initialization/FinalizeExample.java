a61dd8623b37f446e323e572331c290e1b47ef3c
pete <nishizawa23@gmail.com>
2017-01-11 23:44:47 +0800

    Add FinalizeExample.java for example 5.10

---
+++ src/main/java/initialization/FinalizeExample.java
@@ -0,0 +1,29 @@
+package initialization;
+
+import java.util.*;
+
+class Book{
+
+	String bookname = "hello world";
+
+// this is not sure to call, try more times!!
+	protected void finalize() {
+		System.out.println("book class called finalize()");
+	}
+}
+
+public class FinalizeExample {
+	public static void main (String [] args)
+	{
+		/* code */
+		//FinalizeExample mFinalize = new FinalizeExample();
+		new FinalizeExample();
+		//Book mBook = new Book();
+		new Book();
+		System.gc();
+	}
+// this is not sure to call, try more times!!
+	protected void finalize(){
+		System.out.println("finalizeExample class call finalize()");
+	}
+}
