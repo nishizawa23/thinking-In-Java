003402512313a88d54fbcabb161d4219b146f364
pete <nishizawa23@gmail.com>
2017-01-16 23:16:10 +0800

    Update ExplicitStatic.java for exmaple 5.13

--- src/main/java/initialization/ExplicitStatic.java
+++ src/main/java/initialization/ExplicitStatic.java
@@ -29,8 +29,8 @@ public class ExplicitStatic {
     print("Inside main()");
     Cups.cup1.f(99);  // (1)
   }
-  // static Cups cups1 = new Cups();  // (2)
-  // static Cups cups2 = new Cups();  // (2)
+   static Cups cups1 = new Cups();  // (2)
+   static Cups cups2 = new Cups();  // (2)
 } /* Output:
 Inside main()
 Cup(1)
