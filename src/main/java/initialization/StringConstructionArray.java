7001d2e9b80f5a57a8676e02b5d86ad89cc52d03
Hunagxh <witshxh@163.com>
2017-01-17 09:53:33 +0000

    Add StringConstructionArray.java for example 5.17

---
+++ src/main/java/initialization/StringConstructionArray.java
@@ -0,0 +1,18 @@
+package initialization;
+
+import java.util.*;
+
+class Test {
+	Test(String s){
+		System.out.println("Test construct with string " + s);
+	}
+}
+
+public class StringConstructionArray {
+	public static void main (String [] args)
+	{
+		/* code */
+		System.out.println("main start");
+		Test [] mTestArray;
+	}
+}
