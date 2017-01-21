362c9b4e436dfd20854600b2b4640283349a61c4
pete <nishizawa23@gmail.com>
2017-01-21 22:37:08 +0800

    Make dir exercise and add exercise1.java for example 6.1

---
+++ src/main/java/access/exercise/TestPackage.java
@@ -0,0 +1,9 @@
+package access.exercise;
+
+import java.util.*;
+
+public class TestPackage {
+	public TestPackage(){
+		System.out.println("Hello world");
+	}
+}
---
+++ src/main/java/access/exercise1.java
@@ -0,0 +1,12 @@
+package access;
+
+import java.util.*;
+import access.exercise.*;
+
+public class exercise1 {
+	public static void main (String [] args)
+	{
+		/* code */
+		new TestPackage();
+	}
+}
