c9949b009c02828e81564086c5523edcb9333b17
pete <nishizawa23@gmail.com>
2017-01-21 17:25:05 +0800

    Update PaperCurrencyEnum.java for example 5.22

--- src/main/java/initialization/PaperCurrencyEnum.java
+++ src/main/java/initialization/PaperCurrencyEnum.java
@@ -13,5 +13,32 @@ public class PaperCurrencyEnum {
 		for(Memony m : Memony.values()){
 			System.out.println(m + " , ordinal " + m.ordinal());
 		}
+
+		new PaperCurrencyEnum().testEnum(Memony.TWO);
+	}
+
+	private void testEnum(Memony m){
+		switch (m) {
+			case ONE:
+				System.out.println(m + " , ordinal " + m.ordinal());
+				break;
+			case TWO:
+				System.out.println(m + " , ordinal " + m.ordinal());
+				break;
+			case THREE:
+				System.out.println(m + " , ordinal " + m.ordinal());
+				break;
+			case FOUR:
+				System.out.println(m + " , ordinal " + m.ordinal());
+				break;
+			case FIVE:
+				System.out.println(m + " , ordinal " + m.ordinal());
+				break;
+			case SIX:
+				System.out.println(m + " , ordinal " + m.ordinal());
+				break;
+			default:
+				System.out.println("at default");
+		}
 	}
 }
