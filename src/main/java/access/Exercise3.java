940f4d09ed91f0171358bc644a534370439a7657
pete <nishizawa23@gmail.com>
2017-01-24 00:03:57 +0800

    Add Exercise3.java for example 6.3

---
+++ src/main/java/access/Exercise3.java
@@ -0,0 +1,13 @@
+package access;
+
+import java.util.*;
+//import static access.debug.Debug.*;
+import static access.debugoff.Debug.*;
+
+public class Exercise3 {
+	public static void main (String [] args)
+	{
+		/* code */
+		debug("hello world");
+	}
+}
---
+++ src/main/java/access/debug/Debug.java
@@ -0,0 +1,9 @@
+package access.debug;
+
+import java.util.*;
+
+public class Debug {
+	public static void debug(String s){
+		System.out.println("debug String s " + s);
+	}
+}
