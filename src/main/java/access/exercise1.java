ec8c261c18cb8516cbfe532f7792da23289c48fe
pete <nishizawa23@gmail.com>
2017-01-23 23:40:14 +0800

    Update exercise1.java for example 6.2

--- src/main/java/access/exercise1.java
+++ src/main/java/access/exercise1.java
@@ -1,12 +1,17 @@
 package access;
 
-import java.util.*;
+//import java.util.*;
 import access.exercise.*;
+
+import net.mindview.simple.*;
 
 public class exercise1 {
 	public static void main (String [] args)
 	{
 		/* code */
 		new TestPackage();
+		Vector mv = new Vector();
+		java.util.Vector v = new java.util.Vector();
+//		net.mindview.simple.Vector mV = net.mindview.simple.Vector(); 
 	}
 }
