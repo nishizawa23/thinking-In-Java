3aa153ed9026e2874c7f09ad4d96aee0c28be90f
pete <nishizawa23@gmail.com>
2017-01-27 17:24:58 +0800

    Add PackagedClass.java & Foreign.java for example 6.9

---
+++ src/main/java/access/foreign/Foreign.java
@@ -0,0 +1,11 @@
+package access.foreign;
+
+import access.local.*;
+
+public class Foreign {
+	public static void main (String [] args)
+	{
+		/* code */
+		PackagedClass pc = new PackagedClass();
+	}
+}
---
+++ src/main/java/access/local/PackagedClass.java
@@ -0,0 +1,8 @@
+package access.local;
+
+//class PackagedClass { // this line meam package access for class
+public class PackagedClass {
+	public PackagedClass(){
+		System.out.println("Creating a packaged class");
+	}
+}
