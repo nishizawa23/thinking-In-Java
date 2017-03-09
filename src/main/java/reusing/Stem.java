4ab4a3402af92a00579359fe95cedbffdf7c892a
pete <nishizawa23@gmail.com>
2017-03-09 23:01:29 +0800

    Update Stem.java for reusing example 7.10

--- src/main/java/reusing/Stem.java
+++ src/main/java/reusing/Stem.java
@@ -3,20 +3,20 @@ package reusing;
 import java.util.*;
 
 class Component1{
-	Component1(){
-		System.out.println("Component1 constructor");
+	Component1(int i){
+		System.out.println("Component1 constructor i "+i);
 	}
 }
 
 class Component2{
-	Component2(){
-		System.out.println("Component2 constructor");
+	Component2(int i){
+		System.out.println("Component2 constructor i "+i);
 	}
 }
 
 class Component3{
-	Component3(){
-		System.out.println("Component3 constructor");
+	Component3(int i){
+		System.out.println("Component3 constructor i "+i);
 	}
 }
 
@@ -24,25 +24,26 @@ class Root {
 	Component1 mC1;
 	Component2 mC2;
 	Component3 mC3;
-	Root(){
-		System.out.println("Root constructor");
-		mC1 = new Component1();
-		mC2 = new Component2();
-		mC3 = new Component3();
+	Root(int i){
+		System.out.println("Root constructor i "+i);
+		mC1 = new Component1(i);
+		mC2 = new Component2(i);
+		mC3 = new Component3(i);
 	}
 }
 
 public class Stem extends Root {
-	Stem(){
-		System.out.println("Stem constructor");
-		mC1 = new Component1();
-		mC2 = new Component2();
-		mC3 = new Component3();
+	Stem(int i){
+		super(i);
+		System.out.println("Stem constructor i " + i);
+		mC1 = new Component1(12);
+		mC2 = new Component2(12);
+		mC3 = new Component3(12);
 	}
 	public static void main (String [] args)
 	{
 		/* code */
-		Stem mS = new Stem();
+		Stem mS = new Stem(11);
 	}
 }
 
