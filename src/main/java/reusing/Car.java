651b62c9f1d70a0f313fcb348b7bcf03fd3876cc
Hunagxh <witshxh@163.com>
2017-03-11 08:34:45 +0000

    Update Car.java for reusing example 7.14

--- src/main/java/reusing/Car.java
+++ src/main/java/reusing/Car.java
@@ -6,6 +6,9 @@ class Engine {
   public void start() {}
   public void rev() {}
   public void stop() {}
+  public void service() {
+	System.out.println("Engine server");
+}
 }
 
 class Wheel {
@@ -37,5 +40,6 @@ public class Car {
     Car car = new Car();
     car.left.window.rollup();
     car.wheel[0].inflate(72);
+    car.engine.service();
   }
 } ///:~
