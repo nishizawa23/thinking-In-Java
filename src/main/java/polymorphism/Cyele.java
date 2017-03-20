139ab4ed96ff483edc782c5b5c287d17cc4695ed
Hunagxh <witshxh@163.com>
2017-03-20 08:41:21 +0000

    Update Cyele.java for polymorphism exameple 8.5

--- src/main/java/polymorphism/Cyele.java
+++ src/main/java/polymorphism/Cyele.java
@@ -3,18 +3,27 @@ package polymorphism;
 import java.util.*;
 
 class Unicycle extends Cyele {
+	public int wheels(){
+		return 1;
+	}
 	public void go(){
 		System.out.println("Unicycle go");
 	}
 }
 
 class Bicycle extends Cyele {
+	public int wheels(){
+		return 2;
+	}
 	public void go(){
 		System.out.println("Bicycle go");
 	}
 }
 
 class Tricycle extends Cyele {
+	public int wheels(){
+		return 3;
+	}
 	public void go(){
 		System.out.println("Tricycle go");
 	}
@@ -22,8 +31,13 @@ class Tricycle extends Cyele {
 
 public class Cyele {
 
+	public int wheels(){
+		return 0;
+	}
+
 	public static void ride(Cyele c){
 		c.go();
+		System.out.println("go for " + c.wheels() + " wheels");
 	}
 
 	public void go(){
