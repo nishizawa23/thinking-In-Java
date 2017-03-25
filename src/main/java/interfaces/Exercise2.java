5a022597defc9443cabfd1894a527901535f7c33
pete <nishizawa23@gmail.com>
2017-03-25 21:09:35 +0800

    Add Exercise2.java for Interfaces example 9.2

---
+++ src/main/java/interfaces/Exercise2.java
@@ -0,0 +1,17 @@
+package interfaces;
+
+import java.util.*;
+
+abstract class A {
+	public void hello(){
+		System.out.println("hello world");
+	}
+}
+
+class Exercise2 {
+	public static void main (String [] args)
+	{
+		/* code */
+		A a = new A();
+	}
+}
