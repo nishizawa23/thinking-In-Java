dbd82c165f39b5753549a55758ab946bbeb22c24
Hunagxh <witshxh@163.com>
2017-03-29 09:26:15 +0000

    Update Exercise2.java for innerclasses example 9.3

--- src/main/java/innerclasses/Exercise2.java
+++ src/main/java/innerclasses/Exercise2.java
@@ -3,7 +3,19 @@ package innerclasses;
 import java.util.*;
 
 class Outer {
-	private static final String mString = "heloo world";
+	private static String mString;
+
+	class Inner {
+		@Override
+		public String toString(){
+			return mString;
+		}
+	}
+
+	Outer(){
+		mString = "hello world!";
+	}
+
 	@Override
 	public String toString(){
 		return mString;
@@ -14,10 +26,11 @@ class Exercise2 {
 	public static void main (String [] args)
 	{
 		/* code */
+		Outer.Inner [] mInners = new Outer.Inner[10];
 		Outer [] mOuters = new Outer[10];
 		for(Outer o : mOuters){
 			o = new Outer();
-			System.out.println(o);
+			System.out.println(o.new Inner());
 		}
 	}
 }
