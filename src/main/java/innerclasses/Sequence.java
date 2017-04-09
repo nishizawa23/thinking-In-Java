d68f1cb9b9d20133faeaccdf8696fd54d6575f0f
pete <nishizawa23@gmail.com>
2017-04-09 23:53:51 +0800

    Update Sequence.java for holding example 10.3

--- src/main/java/innerclasses/Sequence.java
+++ src/main/java/innerclasses/Sequence.java
@@ -1,5 +1,7 @@
 //: innerclasses/Sequence.java
 package innerclasses; /* Added by Eclipse.py */
+
+import java.util.*;
 // Holds a sequence of Objects.
 
 interface Selector {
@@ -11,19 +13,22 @@ interface Selector {
 
 public class Sequence {
   private Object[] items;
+  private ArrayList <String>mList;
   private int next = 0;
-  public Sequence(int size) { items = new Object[size]; }
+//  public Sequence(int size) { items = new Object[size]; }
+  public Sequence() { mList = new ArrayList<String>(); }
   public void add(Object x) {
-    if(next < items.length)
-      items[next++] = x;
+//    if(next < items.length)
+//      items[next++] = x;
+		mList.add((String)x);
   }
   private class SequenceSelector implements Selector {
     private int i = 0;
-    public boolean end() { return i == items.length; }
-    public Object current() { return items[i]; }
-    public void next() { if(i < items.length) i++; }
+    public boolean end() { return i == mList.size(); }
+    public String current() { return mList.get(i); }
+    public void next() { if(i < mList.size()) i++; }
 	public Object [] getSequence(){
-		return items;
+		return mList.toArray();
 	}
   }
 
@@ -45,8 +50,9 @@ public class Sequence {
     return new ReverseSelector();
   }	
   public static void main(String[] args) {
-    Sequence sequence = new Sequence(10);
-    for(int i = 0; i < 10; i++)
+    Sequence sequence = new Sequence();
+    //ArrayList <Sequence> mList = new ArrayList<Sequence>();
+    for(int i = 0; i < 20; i++)
       sequence.add(Integer.toString(i));
     Selector selector = sequence.selector();
 
@@ -54,17 +60,18 @@ public class Sequence {
       System.out.print(selector.current() + " ");
       selector.next();
     }
-
+/*
     Selector reverseSelector = sequence.reverseSelector();
     while(!reverseSelector.end()) {
       System.out.print(reverseSelector.current() + " ");
       reverseSelector.next();
     }
+*/
 
 	System.out.println();
 
     Object [] mSS = selector.getSequence();
-	for(int j = 0; j < 10; j++){
+	for(int j = 0; j < mSS.length; j++){
 		System.out.println(mSS[j]);
 	}
   }
