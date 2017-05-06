a8f23ed4725f4b1ad2a7729675db605541180fa2
pete <nishizawa23@gmail.com>
2017-05-06 18:59:58 +0800

    Update RandomShapeGenertor.java for holding exercise 10.31

--- src/main/java/polymorphism/shape/RandomShapeGenerator.java
+++ src/main/java/polymorphism/shape/RandomShapeGenerator.java
@@ -2,9 +2,12 @@
 // A "factory" that randomly creates shapes.
 package polymorphism.shape;
 import java.util.*;
+import polymorphism.shape.RandomShapeGenerator;
 
-public class RandomShapeGenerator {
+public class RandomShapeGenerator implements Iterable<Shape>{
   private Random rand = new Random(47);
+  private int number;
+  private int index = 0;
   public Shape next() {
     switch(rand.nextInt(4)) {
       default:
@@ -13,5 +16,33 @@ public class RandomShapeGenerator {
       case 2: return new Triangle();
       case 3: return new Circle();
     }
+  }
+  RandomShapeGenerator(int n){
+	  number = n;
+  }
+  public Iterator<Shape> iterator(){
+	  return new Iterator<Shape>(){
+
+		  public boolean hasNext(){
+			  if(index<number){
+				  index++;
+				  return true;
+			  }else{
+				  return false;
+			  }
+		  }
+	  	public Shape next(){
+			  return new Car();
+	  	}
+		public void remove(){
+			throw new UnsupportedOperationException();
+		}
+	  };
+  }
+  public static void main(String[] args) {
+	  RandomShapeGenerator mR = new RandomShapeGenerator(8);
+	  for(Shape s : mR )
+		  System.out.println("Shape " + s);
+  	
   }
 } ///:~
