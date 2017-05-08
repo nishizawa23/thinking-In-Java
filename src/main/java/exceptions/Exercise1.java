fe6949dc308dda22bd1da25eb66d5f5df1dc83a2
pete <nishizawa23@gmail.com>
2017-05-08 22:49:39 +0800

    Add Exercise1.java for exceptions execise 11.1

---
+++ src/main/java/exceptions/Exercise1.java
@@ -0,0 +1,26 @@
+package exceptions;
+
+import exceptions.Exercise1;
+import java.util.*;
+
+class MyException extends Exception {
+	MyException(String mString){
+		super(mString);
+	}
+}
+
+public class Exercise1{
+
+	public void f() throws MyException{
+		throw new MyException("hello");
+	}
+
+	public static void main(String[] args) {
+		try {
+			new Exercise1().f();
+		} catch(MyException e){
+			e.printStackTrace(System.out);
+		}
+		
+	}
+} 
