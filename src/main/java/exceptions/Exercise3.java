67d7500006b8b205a63c8c429d2b4c5f85f7785a
pete <nishizawa23@gmail.com>
2017-07-04 22:49:47 +0800

    Update Exercise3.java for exceptions execise 11.7

--- src/main/java/exceptions/Exercise3.java
+++ src/main/java/exceptions/Exercise3.java
@@ -1,17 +1,30 @@
 package exceptions;
 
 import java.util.*;
+import java.util.logging.*;
+import java.io.*;
 
-public class Exercise3{
-	public static void main(String[] args) {
-		int [] mIntArray= new int [3];
-		try {
-			mIntArray[3] = 10;
-			
-		} catch(ArrayIndexOutOfBoundsException e) {
-			e.printStackTrace();
-		} finally {
-			System.out.println("finally hello");
-		}
-	}
-} 
+class ExceptionOne extends Exception {
+        private static Logger logger = Logger.getLogger("ExceptionOne");
+
+        static void logExceptionOne(Exception e) {
+                StringWriter trace = new StringWriter();
+                e.printStackTrace(new PrintWriter(trace));
+                logger.severe(trace.toString());
+        }
+}
+
+public class Exercise3 {
+        public static void main(String[] args) {
+                int [] mIntArray = new int [3];
+                try {
+                        mIntArray[3] = 10;
+
+                } catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("i am going to ExceptionOne");
+                        ExceptionOne.logExceptionOne(e);
+                } finally {
+                        System.out.println("finally hello");
+                }
+        }
+}
