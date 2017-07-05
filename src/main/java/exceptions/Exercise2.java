56f842c5bc0d3c3eae485edb2de1eb3b48f48f96
pete <nishizawa23@gmail.com>
2017-07-05 22:52:15 +0800

    Update Exercise2.java for exceptions execise 11.8

--- src/main/java/exceptions/Exercise2.java
+++ src/main/java/exceptions/Exercise2.java
@@ -3,22 +3,45 @@ package exceptions;
 import java.util.*;
 
 class Hello {
-	public void sayHelo(){
-		System.out.println("hello world");
-	}
+        public void sayHelo() {
+                System.out.println("hello world");
+        }
 }
 
-public class Exercise2{
-	public Hello mHello = null;
-	public static void main(String[] args) {
-		try {
-			new Exercise2().mHello.sayHelo();
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		} finally {
-			System.out.println("finally hello");
-		}
-		
-	}
-} 
+class MyException extends Exception {
+
+}
+
+class World {
+        void f() throws NullPointerException, MyException {
+                throw new NullPointerException();
+        }
+}
+
+public class Exercise2 {
+        public Hello mHello = null;
+        static private World mWorld = null;
+        public static void main(String[] args) {
+                try {
+                        new Exercise2().mHello.sayHelo();
+
+                } catch (Exception e) {
+                        e.printStackTrace();
+                } finally {
+                        System.out.println("finally hello");
+                }
+
+                mWorld = new World();
+                try {
+
+                        mWorld.f();
+                } catch (MyException e) {
+                        e.printStackTrace();
+                } catch (NullPointerException e) {
+                        e.printStackTrace();
+                } finally {
+                        System.out.println("finally world");
+                }
+
+        }
+}
