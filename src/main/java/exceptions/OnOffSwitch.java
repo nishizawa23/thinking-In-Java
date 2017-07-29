1d682f774aa8c6e90a8b4d50d58490c747d9fbdf
pete <nishizawa23@gmail.com>
2017-07-29 12:48:14 +0800

    Update OnOffSwitch.java for exceptions exercise 11.14

--- src/main/java/exceptions/OnOffSwitch.java
+++ src/main/java/exceptions/OnOffSwitch.java
@@ -3,23 +3,25 @@ package exceptions; /* Added by Eclipse.py */
 // Why use finally?
 
 public class OnOffSwitch {
-  private static Switch sw = new Switch();
-  public static void f()
-  throws OnOffException1,OnOffException2 {}
-  public static void main(String[] args) {
-    try {
-      sw.on();
-      // Code that can throw exceptions...
-      f();
-      sw.off();
-    } catch(OnOffException1 e) {
-      System.out.println("OnOffException1");
-      sw.off();
-    } catch(OnOffException2 e) {
-      System.out.println("OnOffException2");
-      sw.off();
-    }
-  }
+        private static Switch sw = new Switch();
+        public static void f()
+        throws OnOffException1, OnOffException2 {
+                throw new RuntimeException();
+        }
+        public static void main(String[] args) {
+                try {
+                        sw.on();
+                        // Code that can throw exceptions...
+                        f();
+                        sw.off();
+                } catch (OnOffException1 e) {
+                        System.out.println("OnOffException1");
+                        sw.off();
+                } catch (OnOffException2 e) {
+                        System.out.println("OnOffException2");
+                        sw.off();
+                }
+        }
 } /* Output:
 on
 off
