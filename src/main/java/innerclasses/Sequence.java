9867f98387b2969ff15a117ba904ae23b7b5c66e
pete <nishizawa23@gmail.com>
2017-07-29 12:32:36 +0800

    Update innerclasses/Sequence.java for exceptions execise 11.12

--- src/main/java/innerclasses/Sequence.java
+++ src/main/java/innerclasses/Sequence.java
@@ -5,83 +5,101 @@ import java.util.*;
 // Holds a sequence of Objects.
 
 interface Selector {
-  boolean end();
-  Object current();
-  void next();
-  Object [] getSequence();
-}	
+        boolean end();
+        Object current();
+        void next();
+        Object [] getSequence();
+}
 
 public class Sequence {
-  private Object[] items;
-  private ArrayList <String>mList;
-  private int next = 0;
+        private Object[] items;
+        private ArrayList <String>mList;
+        private int next = 0;
+        private static final int MAX_NUM = 10;
 //  public Sequence(int size) { items = new Object[size]; }
-  public Sequence() { mList = new ArrayList<String>(); }
-  public void add(Object x) {
+        public Sequence() {
+                mList = new ArrayList<String>();
+        }
+        public void add(Object x) {
 //    if(next < items.length)
 //      items[next++] = x;
-		mList.add((String)x);
-  }
-  private class SequenceSelector implements Selector {
-    private int i = 0;
-    public boolean end() { return i == mList.size(); }
-    public String current() { return mList.get(i); }
-    public void next() { if(i < mList.size()) i++; }
-	public Object [] getSequence(){
-		return mList.toArray();
-	}
-  }
+                if (mList.size() <= 10)
+                        mList.add((String)x);
+                else
+                        throw new ArrayIndexOutOfBoundsException();
+        }
+        private class SequenceSelector implements Selector {
+                private int i = 0;
+                public boolean end() {
+                        return i == mList.size();
+                }
+                public String current() {
+                        return mList.get(i);
+                }
+                public void next() {
+                        if (i < mList.size()) i++;
+                }
+                public Object [] getSequence() {
+                        return mList.toArray();
+                }
+        }
 
-  private class ReverseSelector implements Selector {
-    private int i = items.length -1;
-    public boolean end() { return i == -1; }
-    public Object current() { return items[i]; }
-    public void next() { if(i >= 0 ) i--; }
-	public Object [] getSequence(){
-		return items;
-	}
-  }
+        private class ReverseSelector implements Selector {
+                private int i = items.length - 1;
+                public boolean end() {
+                        return i == -1;
+                }
+                public Object current() {
+                        return items[i];
+                }
+                public void next() {
+                        if (i >= 0 ) i--;
+                }
+                public Object [] getSequence() {
+                        return items;
+                }
+        }
 
-  public Iterator<String> selector() {
+        public Iterator<String> selector() {
 //    return new SequenceSelector();
-	  return mList.iterator();
-  }	
+                return mList.iterator();
+        }
 
-  public Selector reverseSelector() {
-    return new ReverseSelector();
-  }	
-  public static void main(String[] args) {
-    Sequence sequence = new Sequence();
-    //ArrayList <Sequence> mList = new ArrayList<Sequence>();
-    for(int i = 0; i < 20; i++)
-      sequence.add(Integer.toString(i));
+        public Selector reverseSelector() {
+                return new ReverseSelector();
+        }
+        public static void main(String[] args) {
+                Sequence sequence = new Sequence();
+                //ArrayList <Sequence> mList = new ArrayList<Sequence>();
+                for (int i = 0; i < 20; i++)
+                        sequence.add(Integer.toString(i));
 //    Selector selector = sequence.selector();
-	  Iterator<String> mS = sequence.selector();
-/*
-    while(!selector.end()) {
-      System.out.print(selector.current() + " ");
-      selector.next();
-    }
-*/
-	while(mS.hasNext()){
-		System.out.println(mS.next());
-	}
-/*
-    Selector reverseSelector = sequence.reverseSelector();
-    while(!reverseSelector.end()) {
-      System.out.print(reverseSelector.current() + " ");
-      reverseSelector.next();
-    }
-*/
+                Iterator<String> mS = sequence.selector();
+                /*
+                    while(!selector.end()) {
+                      System.out.print(selector.current() + " ");
+                      selector.next();
+                    }
+                */
+                while (mS.hasNext()) {
+                        System.out.println(mS.next());
+                }
+                /*
+                    Selector reverseSelector = sequence.reverseSelector();
+                    while(!reverseSelector.end()) {
+                      System.out.print(reverseSelector.current() + " ");
+                      reverseSelector.next();
+                    }
+                */
 
-	System.out.println();
-/*
-    Object [] mSS = selector.getSequence();
-	for(int j = 0; j < mSS.length; j++){
-		System.out.println(mSS[j]);
-	}
-*/
-  }
+                System.out.println();
+                /*
+                    Object [] mSS = selector.getSequence();
+                        for(int j = 0; j < mSS.length; j++){
+                                System.out.println(mSS[j]);
+                        }
+                */
+        }
 } /* Output:
 0 1 2 3 4 5 6 7 8 9
 *///:~
