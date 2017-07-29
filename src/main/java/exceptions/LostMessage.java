478e04a88bd65168eff372f6201bf92980e2416c
pete <nishizawa23@gmail.com>
2017-07-29 13:33:07 +0800

    Update LostMessage.java for exceptions exercise 11.18

--- src/main/java/exceptions/LostMessage.java
+++ src/main/java/exceptions/LostMessage.java
@@ -3,36 +3,47 @@ package exceptions; /* Added by Eclipse.py */
 // How an exception can be lost.
 
 class VeryImportantException extends Exception {
-  public String toString() {
-    return "A very important exception!";
-  }
+        public String toString() {
+                return "A very important exception!";
+        }
 }
 
 class HoHumException extends Exception {
-  public String toString() {
-    return "A trivial exception";
-  }
+        public String toString() {
+                return "A trivial exception";
+        }
+}
+
+class ThreeException extends Exception {
+        public String toString() {
+                return "A three exception";
+        }
 }
 
 public class LostMessage {
-  void f() throws VeryImportantException {
-    throw new VeryImportantException();
-  }
-  void dispose() throws HoHumException {
-    throw new HoHumException();
-  }
-  public static void main(String[] args) {
-    try {
-      LostMessage lm = new LostMessage();
-      try {
-        lm.f();
-      } finally {
-        lm.dispose();
-      }
-    } catch(Exception e) {
-      System.out.println(e);
-    }
-  }
+        void f() throws VeryImportantException {
+                throw new VeryImportantException();
+        }
+        void dispose() throws HoHumException, ThreeException {
+                try {
+                        throw new HoHumException();
+                } catch (Exception e) {
+                        System.out.println("get HoHumException");
+                        throw new ThreeException();
+                }
+        }
+        public static void main(String[] args) {
+                try {
+                        LostMessage lm = new LostMessage();
+                        try {
+                                lm.f();
+                        } finally {
+                                lm.dispose();
+                        }
+                } catch (Exception e) {
+                        System.out.println(e);
+                }
+        }
 } /* Output:
 A trivial exception
 *///:~
