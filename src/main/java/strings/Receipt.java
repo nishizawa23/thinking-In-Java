c7a2cf179c73f24c4f9b9dc36700c36a216f50aa
pete <nishizawa23@gmail.com>
2017-07-31 23:11:13 +0800

    Update Receipt.java for strings exercise 11.4

--- src/main/java/strings/Receipt.java
+++ src/main/java/strings/Receipt.java
@@ -5,18 +5,19 @@ import java.util.*;
 public class Receipt {
   private double total = 0;
   private Formatter f = new Formatter(System.out);
+  private int W1 = 20;
   public void printTitle() {
-    f.format("%-15s %5s %10s\n", "Item", "Qty", "Price");
-    f.format("%-15s %5s %10s\n", "----", "---", "-----");
+    f.format("%-"+W1+"s %5s %10s\n", "Item", "Qty", "Price");
+    f.format("%-"+W1+"s %5s %10s\n", "----", "---", "-----");
   }
   public void print(String name, int qty, double price) {
-    f.format("%-15.15s %5d %10.2f\n", name, qty, price);
+    f.format("%-"+W1+"."+W1+"s %5d %10.2f\n", name, qty, price);
     total += price;
   }
   public void printTotal() {
-    f.format("%-15s %5s %10.2f\n", "Tax", "", total*0.06);
-    f.format("%-15s %5s %10s\n", "", "", "-----");
-    f.format("%-15s %5s %10.2f\n", "Total", "",
+    f.format("%-"+W1+"s %5s %10.2f\n", "Tax", "", total*0.06);
+    f.format("%-"+W1+"s %5s %10s\n", "", "", "-----");
+    f.format("%-"+W1+"s %5s %10.2f\n", "Total", "",
       total * 1.06);
   }
   public static void main(String[] args) {
