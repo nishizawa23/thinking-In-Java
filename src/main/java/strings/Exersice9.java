8fb2e582da647a9d8c19900cece635ed91f882e2
Hunagxh <witshxh@163.com>
2017-08-05 03:45:27 +0000

    Add Exercise9.java for strings exercise 13.9

---
+++ src/main/java/strings/Exersice9.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class Exersice9 {
+	  public static String knights =
+			    "Then, when you have found the shrubbery, you must " +
+			    "cut down the mightiest tree in the forest... " +
+			    "with... a herring!";
+	  public static void main(String[] args){
+		  System.out.println(knights.replaceAll("[aeiou|AEIOU]", "_"));
+	  }
+
+}
