724c5ece74b220a000994a8703271dee06bda463
pete <nishizawa23@gmail.com>
2017-08-06 22:44:48 +0800

    Update Exercise8.java for typeinfo exercise 14.09

--- src/main/java/typeinfo/Exercise8.java
+++ src/main/java/typeinfo/Exercise8.java
@@ -1,31 +1,37 @@
 package typeinfo;
 
+import java.lang.reflect.Field;
 import java.util.*;
 
 public class Exercise8 {
 
-        private static void getSuperClass(Class mC,String mClassName){
-                        if(mC.getSuperclass()!=null){
-                                System.out.println("Class : " + mClassName + " has spuer class " + mC.getSuperclass());
-                                for(Class mInterfaceClass:mC.getInterfaces())
-                                        System.out.print("interface "+mInterfaceClass.getCanonicalName());
-                                getSuperClass(mC.getSuperclass(),mC.getSuperclass().getCanonicalName());
-                        }
-        }
+	private static void getSuperClass(Class<?> mC, String mClassName) {
+		if (mC.getSuperclass() != null) {
+			System.out.println("Class : " + mClassName + " has spuer class " + mC.getSuperclass());
+			for (Field mf : mC.getDeclaredFields())
+				System.out.println("Class Field : " + mf.getName());
+			for (Class<?> mInterfaceClass : mC.getInterfaces()) {
+				System.out.print("interface " + mInterfaceClass.getCanonicalName());
+				for (Field mi : mInterfaceClass.getDeclaredFields())
+					System.out.println("Class Field : " + mi.getName());
+			}
+			getSuperClass(mC.getSuperclass(), mC.getSuperclass().getCanonicalName());
+		}
+	}
 
-        public static void main(String[] args) {
-                if (args.length < 1) {
-                        System.out.println("please input args for class");
-                        return;
-                }
-                String mClassName = args[0];
-                try {
-                        Class mC = Class.forName(mClassName);
-                        //Exercise8.getSuperClass(mC,mClassName);
-                        getSuperClass(mC,mClassName);
-                } catch (Exception e) {
-                        e.printStackTrace();
-                }
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("please input args for class");
+			return;
+		}
+		String mClassName = args[0];
+		try {
+			Class<?> mC = Class.forName(mClassName);
+			// Exercise8.getSuperClass(mC,mClassName);
+			getSuperClass(mC, mClassName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        }
+	}
 }
